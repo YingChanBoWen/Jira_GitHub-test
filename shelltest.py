5e2153c9805a220f6c0164f013091ce881c41e15
SongBowen <945745550@qq.com>
2019-07-31 12:55:11 +0800

    [PRO-277] dasdad

--- shelltest.py
+++
@@ -1,8 +0,0 @@
-import os
-
-f = open("log.txt", "w")  
-
-files = os.popen("git log --graph --pretty=format:\'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset\' --abbrev-commit --date=relative --since=\"2019.07.26\" --until=\"2019.07.30\"").readlines() 
-for file_item in files:
-	print >> f, file_item
-f.close()
